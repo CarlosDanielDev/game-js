4dd5f12b3b812bae72bfeb90f5872840bcee1af8
Daniel <danphp7@gmaiol.com>
2018-06-19 17:29:50 -0300

    Aula - JS - aspas nas strings

---
+++ game.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//Adicionando aspasna na string usando contra barra//
+var nome = '\"Daniel\"';
+var nome1 = "Carlos";
+document.write(nome1, '<br>', nome, '<br>');
+
+var nome2 = '\'Matheus\'';
+
+document.write(nome2);
+////////////////////////////////////////////////////
---
+++ gameBabel.js
@@ -0,0 +1,10 @@
+
+//Adicionando aspasna na string usando contra barra//
+let nome = '\"Daniel\"';
+let nome1 = "Carlos";
+document.write(nome1, '<br>', nome, '<br>');
+
+let nome2 = '\'Matheus\'';
+
+document.write(nome2);
+////////////////////////////////////////////////////
