44a99cca3407fc180caf9030895c449eb4045b4c
Daniel <danphp7@gmaiol.com>
2018-06-20 14:59:31 -0300

    Loops JS - ES6 - While e For

--- game.js
+++ game.js
@@ -1,19 +1,14 @@
 'use strict';
 
-//////////////ARRAY/////////////////////
-var lista = ['maçã', 'leite', 'açucar'];
-document.write(lista.length + '<br>'); // return 3
-document.write(lista[2] + '</br>'); // return 3º indice do array listas
-///////Mudar Indice especifico do array/////
-var array = ['Edinei', 'Matheus', 'Daniel'];
-array[0] = 'Luan';
-document.write(array + '<br>'); // Indice 0 retorna Luan ao Inves de Edinei
-////////Excluindo valores do array//////////////////////////
-var array2 = ['Leo', 'Wall', 'Yago'];
-array2.pop(); // remove o ultimo indice do array
-array2.shift(); // remove o primeir indice do array
-array2.unshift('Mara'); // adiciona um indice no começo do array
-array2.push('Luciana'); // adiciona um indice no final do array
-array2.splice(1, 1); // remove um indice do array, primeiro parametro
-document.write(array2 + '<br>');
-////////////////////////////////////////////
+////////WHILE JS - ES6/////////////
+var i = 0;
+while (i <= 10) {
+	// document.write(i + '</br>');
+	i++;
+}
+//////////FOR - JS - ES6 ///////
+var lista = ['Abacate', 'Leite', 'Maçã'];
+
+for (var _i = 0; _i < lista.length; _i++) {
+	document.write(lista[_i] + '<br>');
+}
--- gameBabel.js
+++ gameBabel.js
@@ -1,17 +1,12 @@
-//////////////ARRAY/////////////////////
-let lista = ['maçã', 'leite', 'açucar'];
-document.write(lista.length + '<br>'); // return 3
-document.write(lista[2] + '</br>'); // return 3º indice do array listas
-///////Mudar Indice especifico do array/////
-let array = ['Edinei', 'Matheus', 'Daniel'];
-array[0] = 'Luan';
-document.write(array + '<br>'); // Indice 0 retorna Luan ao Inves de Edinei
-////////Excluindo valores do array//////////////////////////
-let array2 = ['Leo', 'Wall', 'Yago'];
-array2.pop(); // remove o ultimo indice do array
-array2.shift(); // remove o primeir indice do array
-array2.unshift('Mara'); // adiciona um indice no começo do array
-array2.push('Luciana'); // adiciona um indice no final do array
-array2.splice(1, 1); // remove um indice do array, primeiro parametro
-document.write(array2 + '<br>');
-////////////////////////////////////////////
+////////WHILE JS - ES6/////////////
+let i = 0;
+while(i <= 10){
+	// document.write(i + '</br>');
+	i++;
+}
+//////////FOR - JS - ES6 ///////
+let lista = ['Abacate', 'Leite', 'Maçã'];
+
+for (let i = 0; i < lista.length ; i++){
+	document.write(lista[i] + '<br>');
+}
