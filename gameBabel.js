09152e90e838ce5e918db7728b9a67822ef46c2e
Daniel <danphp7@gmaiol.com>
2018-06-20 10:09:19 -0300

    Booleanos, Aritimeticos, E Igualdade

--- gameBabel.js
+++ gameBabel.js
@@ -1,11 +1,19 @@
-///////MANIPULAÇÃO//DE//STRING//////////////////
-let fname = 'Carlos ';
-let mname = ' Daniel';
-let lname = ' S. Oliveira';
-let all = name + mname + lname;
-document.write(all.length + '<br>');
-document.write(all.charAt(1) + '<br>');
-document.write(all.substring(0, 3) + '<br>');
-document.write(all.toLowerCase() + '<br>');
-document.write(all.toUpperCase() + '<br>');
-/////////////////////////////////////////////////
+/////////////BOOLEANOS - JS////////////////
+document.write((5 > 10) + '</br>'); // return False
+document.write((5 < 10) + '</br>'); // return True
+document.write((5 >= 10) + '</br>'); // return False
+document.write((5 <= 10) + '</br>'); // return True
+///////////OPERADORES///ARITIMETICOS//////
+document.write((5 * '10') + '</br>'); // return 50
+document.write((5 + '10') + '</br>'); // return 510
+document.write((11 > '10') + '</br>'); // return True
+/////////OPERADORES DE IGUALDADE/////////////
+document.write((10 == 10) + '</br>'); // retun True
+document.write((10 == '10') + '</br>'); // return True
+document.write((10 != 10) + '</br>'); // return False
+document.write((10 != '10') + '</br>'); // return False
+document.write((10 === 10) + '</br>'); // return True
+document.write((10 === '10') + '</br>'); // return False
+document.write((10 !== '10') + '</br>'); // return True
+document.write((10 !== 10) + '</br>'); // return False
+///////////////////////////////////////////////
