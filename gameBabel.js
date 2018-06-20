fa51f03b2007e9a34b791692824ff9fec999cf2a
Daniel <danphp7@gmaiol.com>
2018-06-20 15:26:47 -0300

    Laço de Repetição SWITCH com JS - ES6

--- gameBabel.js
+++ gameBabel.js
@@ -1,24 +1,15 @@
-///////////// Condições em JS - ES6 ////////////////
-if (3 === 3){
-	document.write('é igual' + '</br>');
-}
+//////////////// Loop Switch JS - ES6 ////////////
+let idade = 17;
 
-let idade = 100;
-if (idade >= 19){
-	document.write('Você Tem '+ idade + ' Anos de Idade' + '<br>');
-} else if (idade >= 18){ 
-	document.write('Maior de Idade' + '<br>');
-}else {
-	document.write('Menor De Idade' + '<br>');
+switch(idade){
+	case 17 :
+		document.write('Tem 17' + '<br>');
+		break;
+	case 18:
+		document.write('Tem 18' + '<br>');
+		break;
+	default:
+		document.write('ERROR' + '<br>');
+		break;
 }
-
-////////////Combinando Loops com condições ///////////
-
-for (let i = 0; i < 10; i++) {
-	if (i === 3) {
-	//	break; para o laço na hora
-		continue; // continua o laço ate o fim
-	}
-	document.write(i + '</br>');
-}
-//////////////////////////////////////
+////////////////////////////////////////////////
